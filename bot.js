5f1873d0986954d2c2d564acc01d19e23069b3ec
arise005 <67866403+arise005@users.noreply.github.com>
2020-07-05 09:12:14 -0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,34 @@
+{
+  "ClientId": 728412370560352289,
+  "Token": "NzI4NDEyMzcwNTYwMzUyMjg5.Xv6BhA.HUX7GqGBBu2yUMz_EEz_KLzY9aU",
+  "OwnerIds": [
+    257679192215388160
+  ],
+  "LoLApiKey": "",
+  "GoogleApiKey": "",
+  "MashapeKey": "",
+  "OsuApiKey": "",
+  "SoundCloudClientId": "",
+  "CleverbotApiKey": "",
+  "CarbonKey": "",
+  "Db": {
+    "Type": "sqlite",
+    "ConnectionString": "Data Source=data/NadekoBot.db"
+  },
+  "TotalShards": 1,
+  "PatreonAccessToken": "",
+  "PatreonCampaignId": "334038",
+  "RestartCommand": null,
+  "ShardRunCommand": "",
+  "ShardRunArguments": "",
+  "ShardRunPort": null,
+  "MiningProxyUrl": null,
+  "MiningProxyCreds": null,
+  "BotListToken": null,
+  "TwitchClientId": null,
+  "VotesToken": null,
+  "VotesUrl": null,
+  "RedisOptions": null,
+  "LocationIqApiKey": null,
+  "TimezoneDbApiKey": null
+}
